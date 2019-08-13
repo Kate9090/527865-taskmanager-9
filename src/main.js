e65d1d12bc8966bd6fbb1a32f99357d1707e07c8
eremina-ekaterina <eremina@uchi.ru>
2019-08-13 10:56:37 +0300

    prove files

--- src/main.js
+++ src/main.js
@@ -8,31 +8,35 @@ import {makeSearchTemplate} from '../src/components/search';
 
 const CARDS_COUNT = 3;
 
-const renderComponent = (parent, child) => {
-  parent.insertAdjacentHTML(`beforeend`, child);
+const renderComponent = (parent, child, place) => {
+  parent.insertAdjacentHTML(place, child);
 };
 
 const mainContainer = document.querySelector(`.main`);
-const menuContainer = mainContainer.querySelector(`.control__btn-wrap`);
-const searchContainer = mainContainer.querySelector(`.main__search`);
-const filterContainer = mainContainer.querySelector(`.main__filter`);
-const cardFilterContainer = mainContainer.querySelector(`.board__tasks-filter`);
-const cardTasksContainer = mainContainer.querySelector(`.board__tasks`);
-const cardTasksLoadBtnContainer = mainContainer.querySelector(`.board.container`);
+const menuContainer = mainContainer.querySelector(`.main__control`);
+// const searchContainer = mainContainer.querySelector(`.main__search`);
+// const filterContainer = mainContainer.querySelector(`.main__filter`);
+
+// const cardTasksLoadBtnContainer = mainContainer.querySelector(`.board.container`);
 
 const renderMockComponents = () => {
-  renderComponent(cardTasksContainer, makeCardEditTemplate());
-  renderComponent(menuContainer, makeMenuTemplate());
-  renderComponent(searchContainer, makeSearchTemplate());
-  renderComponent(filterContainer, makeFilterTemplate());
+  renderComponent(menuContainer, makeMenuTemplate(), `beforeend`);
+  renderComponent(mainContainer, makeSearchTemplate(), `beforeend`);
+  renderComponent(mainContainer, makeFilterTemplate(), `beforeend`);
 
-  renderComponent(cardFilterContainer, makeCardFilterTemplate());
+  renderComponent(mainContainer, makeCardEditTemplate(),`beforeend`);
+
+  const cardFilterContainer = mainContainer.querySelector(`.board`);
+  const cardTasksContainer = cardFilterContainer.querySelector(`.board__tasks`);
+
+  renderComponent(cardFilterContainer, makeCardFilterTemplate(), `afterbegin`);
+
 
   for (let i = 1; i <= CARDS_COUNT; i++) {
-    renderComponent(cardTasksContainer, makeCardTemplate());
+    renderComponent(cardTasksContainer, makeCardTemplate(), `beforeend`);
   }
 
-  renderComponent(cardTasksLoadBtnContainer, makeLoadMoreTemplate());
+  renderComponent(cardFilterContainer, makeLoadMoreTemplate(), `beforeend`);
 };
 
 renderMockComponents();
