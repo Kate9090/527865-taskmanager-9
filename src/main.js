e488e19153430b489274cabf4d300466dcdeaca7
eremina-ekaterina <eremina@uchi.ru>
2019-08-15 12:36:06 +0300

    Edit bugs after tutor comment

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import {makeCardEditTemplate} from '../src/components/card-edit'; //+
+import {makeCardEditTemplate} from '../src/components/card-edit';
 import {makeCardFilterTemplate} from '../src/components/card-filter';
 import {makeCardTemplate} from '../src/components/card';
-import {makeFilterTemplate} from '../src/components/filter'; //+
+import {makeFilterTemplate} from '../src/components/filter';
 import {makeLoadMoreTemplate} from '../src/components/load-more';
-import {makeMenuTemplate} from '../src/components/menu'; //+
+import {makeMenuTemplate} from '../src/components/menu';
 import {makeSearchTemplate} from '../src/components/search';
 
 const CARDS_COUNT = 3;
@@ -14,28 +14,20 @@ const renderComponent = (parent, child, place) => {
 
 const mainContainer = document.querySelector(`.main`);
 const menuContainer = mainContainer.querySelector(`.main__control`);
-// const searchContainer = mainContainer.querySelector(`.main__search`);
-// const filterContainer = mainContainer.querySelector(`.main__filter`);
-
-// const cardTasksLoadBtnContainer = mainContainer.querySelector(`.board.container`);
 
 const renderMockComponents = () => {
   renderComponent(menuContainer, makeMenuTemplate(), `beforeend`);
   renderComponent(mainContainer, makeSearchTemplate(), `beforeend`);
   renderComponent(mainContainer, makeFilterTemplate(), `beforeend`);
-
   renderComponent(mainContainer, makeCardEditTemplate(),`beforeend`);
 
   const cardFilterContainer = mainContainer.querySelector(`.board`);
   const cardTasksContainer = cardFilterContainer.querySelector(`.board__tasks`);
 
   renderComponent(cardFilterContainer, makeCardFilterTemplate(), `afterbegin`);
-
-
   for (let i = 1; i <= CARDS_COUNT; i++) {
     renderComponent(cardTasksContainer, makeCardTemplate(), `beforeend`);
   }
-
   renderComponent(cardFilterContainer, makeLoadMoreTemplate(), `beforeend`);
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,5 @@ module.exports = {
     publicPath: 'http://localhost:8080/',
     compress: true,
     watchContentBase: true
-    }
+  }
 };
